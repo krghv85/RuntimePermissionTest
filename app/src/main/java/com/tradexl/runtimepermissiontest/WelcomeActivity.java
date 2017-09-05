989b3fd3e89abe54569c03c7498bcc1c2e5bcefa
Komal Raghav <k.rghv85@gmail.com>
2017-09-05 15:47:13 +0530

    code

---
+++ app/src/main/java/com/tradexl/runtimepermissiontest/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package com.tradexl.runtimepermissiontest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Raghav on 05-Sep-17.
+ */
+
+public class WelcomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+    }
+}
